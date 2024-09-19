bb5c6c211d7fbd4eed05e056bdc96e3fcfe72fb1
ulitsaRaskolnikova <malyshev.2005n@gmail.com>
2024-09-20 01:19:12 +0300

    feat: add l1-12

--- l1-12/src/main.rs
+++ l1-12/src/main.rs
@@ -1,3 +1,21 @@
+use std::collections::HashSet;
+
+fn find_intersection(set1: &HashSet<i32>, set2: &HashSet<i32>) -> HashSet<i32> {
+    let mut intersection = HashSet::new();
+    
+    for &item in set1 {
+        // Если элемент есть во втором множестве, добавляем его в результат
+        if set2.contains(&item) {
+            intersection.insert(item);
+        }
+    }
+    
+    intersection
+}
+
 fn main() {
-    println!("Hello, world!");
+    let set1: HashSet<i32> = vec![1, 2, 3, 4, 5].into_iter().collect();
+    let set2: HashSet<i32> = vec![3, 4, 5, 6, 7].into_iter().collect();
+    let intersection = find_intersection(&set1, &set2);
+    println!("Пересечение: {:?}", intersection);
 }
---
+++ l1-12/target/debug/l1-12.d
@@ -0,0 +1,1 @@
+/Users/admin/rust-projects/wb-l1-tasks/l1-12/target/debug/l1-12: /Users/admin/rust-projects/wb-l1-tasks/l1-12/src/main.rs
