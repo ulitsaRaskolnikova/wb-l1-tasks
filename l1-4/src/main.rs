a472e6a29bb394222d48a8423b0ff8ca5c04d087
ulitsaRaskolnikova <malyshev.2005n@gmail.com>
2024-09-19 16:57:10 +0300

    fix: l1-4

--- l1-4/src/main.rs
+++ l1-4/src/main.rs
@@ -1,48 +1,61 @@
-use std::sync::{Arc, Mutex};
-use std::sync::mpsc;
-use std::thread;
-use std::io;
-
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 fn main() {
-    let mut workers = String::new();
-
-    io::stdin()
-        .read_line(&mut workers)
-        .expect("Failed to read line");
-
-    let workers: u32 = workers.trim().parse().unwrap();
-
-    let (sender, receiver) = mpsc::channel();
-
-    let receiver = Arc::new(Mutex::new(receiver));
-
-    let mut handles = vec![];
-    for i in 0..workers {
-        let receiver = Arc::clone(&receiver);
-        let handle = thread::spawn(move || {
-            consume_messages(i, receiver);
-        });
-        handles.push(handle);
-    }
+    let pool = ThreadPool::new(5);
     let mut i = 0;
     loop {
         let message = format!("Message {}", i);
-        sender.send(message).unwrap();
+        pool.execute(message);
         i += 1;
-        thread::sleep(std::time::Duration::from_millis(500));
     }
 }
 
-fn consume_messages(id: u32, receiver: Arc<Mutex<mpsc::Receiver<String>>>) {
-    // слушаем главный канал, пока что-то получаем от него
-    loop {
-        let message = receiver.lock().unwrap().recv();
-        match message {
-            Ok(message) => {
-                println!("Consumer {} received: {}", id, message); 
-                thread::sleep(std::time::Duration::from_secs(1));
-            }
-            Err(_) => break,
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<String>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
+
+        ThreadPool { workers, sender }
+    }
+    pub fn execute(&self, message: String) {
+        self.sender.send(message).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<String>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
+            match message {
+                Ok(message) => {
+                    println!("Consumer {} received: {}", id, message); 
+                    thread::sleep(std::time::Duration::from_secs(1));
+                }
+                Err(_) => break,
+            }
+        });
+
+        Worker { id, thread }
     }
 }
